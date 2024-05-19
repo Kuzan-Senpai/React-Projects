df3510d1d6239ad90c7b318e40684a7fc796a012
Your Name <oghenetegafrancis@gmail.com>
2024-05-19 22:22:33 +0100

    bgChanger

--- tailwindprops/src/component/Card.jsx
+++ tailwindprops/src/component/Card.jsx
@@ -5,7 +5,7 @@ function Card({username, post, myNum}) {
     return (
         <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 m-10" >
             <img className="w-24 h-24 rounded-full mx-auto" src="https://images.unsplash.com/photo-1618939404235-8747e5c37089?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2dyYW1lcnxlbnwwfHwwfHx8MA%3D%3D" alt="" width="384" height="512"/>
-            <div className="pt-6 text-center space-y-4">
+            <div className="p-6 text-center space-y-4">
                 <blockquote>
                     {myNum}
                     <p className="text-lg font-medium">
